2758fb1347a1ed89db062ebc1795cac1bb775411
Popular234 <cainjose24@g.crookcountyschools.org>
2022-11-01 12:17:15 -0700

    added and modified javascript file, html, and added loops

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("herllo wrld")
+
+for(i=1; i<=10; i=i+2){
+    console.log(i)
+}
+
+const dog = ["dogs ", "love ", "the ", "meats"];
+
+for(i=0; i<dog.length; i++){
+    document.write(dog[i]);
+}
+
+while(i<= 25){
+    console.log(i);
+    i++;
+}
+
+const yup = ["yup", "yeup", "nop", "sure"]
+
+var i = 0
+while(yup[i]){
+    console.log(yup[i]);
+    i++;
+}
